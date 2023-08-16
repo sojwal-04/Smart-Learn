e63e9ae67b2aa7496c79dca23e057f75439ff718
sojwal-04 <sojwalgosavi1999@gmail.com>
2023-08-16 23:19:20 +0530

    Created intial pages and components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import "./index.css"
+
+function App() {
+
+
+  return (
+    <div >
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
